770271894a399c62ba24ae6a018e22fa27ed07b9
Paul Deon Foli <astrosteck@gmail.com>
2026-05-20 08:42:12 -0700

    Added a  background image to the change password page.

--- Episilion-hostels/src/AboutUs/AboutUsPage.jsx
+++ Episilion-hostels/src/AboutUs/AboutUsPage.jsx
@@ -1,119 +1,156 @@
-import { useEffect, useState } from 'react'
-import { PageHeader } from '../PageHeader/PageHeader'
-import { SiteFooter } from '../SiteFooter/SiteFooter'
-import axios from 'axios'
-import { Link } from 'react-router-dom'
-import { TeamCards } from './TeamCards'
-import './AboutUsPage.css'
+import { useEffect, useState } from "react";
+import { PageHeader } from "../PageHeader/PageHeader";
+import { SiteFooter } from "../SiteFooter/SiteFooter";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import { TeamCards } from "./TeamCards";
+import "./AboutUsPage.css";
 
-import facebook from '../assets/icons/contact-us-facebook.png'
-import email from '../assets/icons/contact-us-gmail.png'
-import phone from '../assets/icons/contact-us-phone.png'
-import whatsapp from '../assets/icons/contact-us-whatsapp.png'
-
+import facebook from "../assets/icons/contact-us-facebook.png";
+import email from "../assets/icons/contact-us-gmail.png";
+import phone from "../assets/icons/contact-us-phone.png";
+import whatsapp from "../assets/icons/contact-us-whatsapp.png";
 
 export function AboutUsPage() {
-    const [teamData, setTeamData] = useState([]);
+  const [teamData, setTeamData] = useState([]);
 
-    const loadTeamCards = async () => {
-        const reposnse = await axios.get('https://episilion-backend-2lt0.onrender.com/api/teamMembers');
-        setTeamData(reposnse.data.teamMembers);
-    }
+  const loadTeamCards = async () => {
+    const reposnse = await axios.get(
+      "https://episilion-backend-2lt0.onrender.com/api/teamMembers",
+    );
+    setTeamData(reposnse.data.teamMembers);
+  };
 
-    useEffect(() => {
-        loadTeamCards();
-    }, [])
+  useEffect(() => {
+    loadTeamCards();
+  }, []);
 
+  return (
+    <>
+      <title>About Us | Episilion Hostels</title>
+      {/* <PageHeader navlink={navlink} setNavLink={setNavLink} /> */}
 
+      <main className="about-main">
+        <section className="about-hero">
+          <h2 className="primary-header">About Episilion</h2>
+          <p className="about-paragraph">
+            Helping students find, safe, afforadable, and comfortable, hostels
+            with ease
+          </p>
+        </section>
 
-    return (
-        <>
-            <title>About Us | Episilion Hostels</title>
-            {/* <PageHeader navlink={navlink} setNavLink={setNavLink} /> */}
+        <section className="about-project">
+          <h3>Who We Are</h3>
+          <p className="about-paragraph">
+            Episilion is a student-focused hostel discovery platform designed to
+            simplify The process of finding accommodation. We aim to connect
+            students with verified, Affordable, and quality hostels in just a
+            few clicks.
+          </p>
+        </section>
 
-            <main className='about-main'>
-                <section className="about-hero">
-                    <h2 className="primary-header">About Episilion</h2>
-                    <p className="about-paragraph">Helping students find, safe, afforadable, and comfortable, hostels with ease</p>
-                </section>
+        <section className="mission-vision">
+          <div className="mission">
+            <h3>Our Mission</h3>
+            <p className="about-paragraph">
+              To make hostel searching simple, transparent, and stress-free for
+              students.
+            </p>
+          </div>
 
-                <section className="about-project">
-                    <h3>Who We Are</h3>
-                    <p className="about-paragraph">
-                        Episilion is a student-focused hostel discovery platform designed to simplify
-                        The process of finding accommodation. We aim to connect students with verified,
-                        Affordable, and quality hostels in just a few clicks.
-                    </p>
-                </section>
+          <div className="”vision”">
+            <h3>Our Vision</h3>
+            <p className="about-paragraph">
+              To become the leading digital hostel marketplace for students
+              nationwide.
+            </p>
+          </div>
+        </section>
 
-                <section className="mission-vision">
-                    <div className="mission">
-                        <h3>Our Mission</h3>
-                        <p className="about-paragraph">
-                            To make hostel searching simple, transparent, and stress-free for students.
-                        </p>
-                    </div>
+        <section className="”services”">
+          <h3>What We Offer</h3>
+          <ul className="about-paragraph">
+            <li>Hostel listings by gender</li>
+            <li>Price filtering</li>
+            <li>Verified hostel information</li>
+            <li>Direct contact with hostel management</li>
+          </ul>
+        </section>
 
-                    <div className="”vision”">
-                        <h3>Our Vision</h3>
-                        <p className="about-paragraph">
-                            To become the leading digital hostel marketplace for students nationwide.
-                        </p>
-                    </div>
-                </section>
+        <section className="team-section">
+          <h2 className="primary-header">Meet the Team</h2>
+          <div className="team-cards">
+            {teamData.map((teamMember) => {
+              //WITH THIS FOR EACH TEAM MEMEBER IT WILL GENERATE A TeamCards COMPONENT
+              return <TeamCards key={teamData.Name} teamMember={teamMember} />;
+            })}
+          </div>
+        </section>
 
-                <section className="”services”">
-                    <h3>What We Offer</h3>
-                    <ul className="about-paragraph">
-                        <li>Hostel listings by gender</li>
-                        <li>Price filtering</li>
-                        <li>Verified hostel information</li>
-                        <li>Direct contact with hostel management</li>
-                    </ul>
-                </section>
+        <section>
+          <h2 className="primary-header">Contact Us</h2>
+          <div className="contact-us-container">
+            <p className="contact-us-paragraph">
+              Report an Issue or List Your Hostel If you notice any incorrect
+              information about a hostel on our platform, or if you have a
+              hostel near UPSA that you would like to have listed, please
+              contact us and our team will assist you.
+            </p>
 
-                <section className="team-section">
-                    <h2 className="primary-header">Meet the Team</h2>
-                    <div className="team-cards">
-                        {teamData.map((teamMember) => {
-                            //WITH THIS FOR EACH TEAM MEMEBER IT WILL GENERATE A TeamCards COMPONENT
-                            return (
-                                <TeamCards key={teamData.Name} teamMember={teamMember} />
-                            )
-                        })}
-                    </div>
-                </section>
+            <div className="contact-items">
+              <a
+                href="https://www.facebook.com/episilionhostels26"
+                className="contact-us-icons-container"
+              >
+                <img
+                  src={facebook}
+                  alt="Facebook"
+                  className="contact-us-icons"
+                />
+                <p className="contact-us-icon-name">EpisilionHostels26.com</p>
+              </a>
+              <a
+                href={`mailto:episilionhostels26@gmail.com?subject=${encodeURIComponent("Hostel Inquiry")}&body=${encodeURIComponent("Hello, I would like to inquire about...")}`}
+                className="contact-us-icons-container"
+              >
+                <img src={email} alt="Email" className="contact-us-icons" />
+                <p className="contact-us-icon-name">
+                  episilionhostels26@gmail.com
+                </p>
+              </a>
 
-                <section>
-                    <h2 className='primary-header'>Contact Us</h2>
-                    <div className='contact-us-container'>
-                        <p className='contact-us-paragraph'>Report an Issue or List Your Hostel
-                            If you notice any incorrect information about a hostel on our platform,
-                            or if you have a hostel near UPSA that you would like to have listed,
-                            please contact us and our team will assist you.
-                        </p>
+              <a href="tel:0537222558" className="contact-us-icons-container">
+                <img src={phone} alt="Phone" className="contact-us-icons" />
+                <p className="contact-us-icon-name">0537222558</p>
+              </a>
+              <a
+                href="https://wa.me/0537222558"
+                className="contact-us-icons-container"
+              >
+                <img
+                  src={whatsapp}
+                  alt="Whatsapp"
+                  className="contact-us-icons"
+                />
+                <p className="contact-us-icon-name">0537222558</p>
+              </a>
+            </div>
+          </div>
+        </section>
 
-                        <div className='contact-items'>
-                            <a href="" className='contact-us-icons-container'><img src={facebook} alt="Facebook" className='contact-us-icons' /><p className='contact-us-icon-name'>Episilion.com</p></a>
-                            <a href="" className='contact-us-icons-container'><img src={email} alt="Email" className='contact-us-icons' /><p className='contact-us-icon-name'>epislion@gmail.com</p></a>
-                            <a href="" className='contact-us-icons-container'><img src={phone} alt="Phone" className='contact-us-icons' /><p className='contact-us-icon-name'>0537222558</p></a>
-                            <a href="" className='contact-us-icons-container'><img src={whatsapp} alt="Whatsapp" className='contact-us-icons' /><p className='contact-us-icon-name'>0537222558</p></a>
-                        </div>
-                    </div>
-
-
-                </section>
-
-                <section className="cta">
-                    <h3>Join Us on Our Journey</h3>
-                    <p className='cta-text'>We’re committed to making student accommodation easier and better.</p>
-                    <div className='browse-hostels-cta-button-container'>
-                        <Link to="/" className="browse-hostels-cta-button">Browse Hostels</Link>
-                    </div>
-
-                </section>
-            </main >
-            <SiteFooter />
-        </>
-    )
+        <section className="cta">
+          <h3>Join Us on Our Journey</h3>
+          <p className="cta-text">
+            We’re committed to making student accommodation easier and better.
+          </p>
+          <div className="browse-hostels-cta-button-container">
+            <Link to="/" className="browse-hostels-cta-button">
+              Browse Hostels
+            </Link>
+          </div>
+        </section>
+      </main>
+      <SiteFooter />
+    </>
+  );
 }
--- Episilion-hostels/src/HostelManagerPage/ChangePasswordPage.jsx
+++ Episilion-hostels/src/HostelManagerPage/ChangePasswordPage.jsx
@@ -9,8 +9,14 @@ export function ChangePasswordPage({ managerIsLoggedIn }) {
   const [hostelMangerNewPaswword, setHostelManagerNewPassword] = useState("");
   const [hostelManagerComfirmPassword, setHostelManagerComfirmPassword] =
     useState("");
-  const [passwordUpdateSuccessfull, setPasswordUpdateSuccessfull] =
-    useState();
+  const [passwordUpdateSuccessfull, setPasswordUpdateSuccessfull] = useState();
+
+  useEffect(() => {
+    document.body.classList.add("body-bg");
+    return () => {
+      document.body.classList.remove("body-bg");
+    };
+  }, []);
 
   if (!managerIsLoggedIn) {
     console.log("User is not logged in. Redirecting...");
@@ -51,24 +57,24 @@ export function ChangePasswordPage({ managerIsLoggedIn }) {
         },
       );
       console.log(response.data);
-      setPasswordUpdateSuccessfull('Update Successful')
+      setPasswordUpdateSuccessfull("Update Successful");
       setTimeout(() => {
-        setPasswordUpdateSuccessfull('')
-      }, 2000)
+        setPasswordUpdateSuccessfull("");
+      }, 2000);
     } catch (error) {
-      setPasswordUpdateSuccessfull('Update not Successful Try Again !!')
+      setPasswordUpdateSuccessfull("Update not Successful Try Again !!");
       setTimeout(() => {
-        setPasswordUpdateSuccessfull('')
-      }, 2000)
+        setPasswordUpdateSuccessfull("");
+      }, 2000);
       console.log("Update failed:", error.response?.data || error.message);
     }
   };
 
   function handleClearForm() {
-  setHostelManagerOldPassword("");
-  setHostelManagerNewPassword("");
-  setHostelManagerComfirmPassword("");
-}
+    setHostelManagerOldPassword("");
+    setHostelManagerNewPassword("");
+    setHostelManagerComfirmPassword("");
+  }
 
   return (
     <>
@@ -77,84 +83,87 @@ export function ChangePasswordPage({ managerIsLoggedIn }) {
         className="hostel-manager-change-password-form"
         onSubmit={updateManagerPassword}
       >
-        <div className="hostel-manager-password-change-header-container">
-          <img
-            src={passwordImage}
-            alt=""
-            className="hostel-manager-password-change-header-image"
-          />
-          <p className="hostel-manager-password-change-header-paragragh">
-            Change password
-          </p>
-          <p className="hostel-manager-password-change-header-second-paragraph">
-            Keep your account secure with a strong password
-          </p>
-        </div>
-
-        <div className="hostel-manager-password-change-main-container">
-          <label
-            for=""
-            className="hostel-manager-password-change-input-headers"
-          >
-            OLD PASSWORD
-          </label>
-          <input
-            type="text"
-            className="hostel-manager-password-change-input"
-            value={hostelManagerOldpassword}
-            onChange={handleOldPassword}
-          />
-
-          <label
-            htmlFor=""
-            className="hostel-manager-password-change-input-headers"
-          >
-            NEW PASSWORD
-          </label>
-          <input
-            type="text"
-            name=""
-            id=""
-            className="hostel-manager-password-change-input"
-            value={hostelMangerNewPaswword}
-            onChange={handleNewPassword}
-          />
-
-          <label
-            htmlFor=""
-            className="hostel-manager-password-change-input-headers"
-          >
-            COMFIRM PASSWORD
-          </label>
-          <input
-            type="text"
-            className="hostel-manager-password-change-input"
-            value={hostelManagerComfirmPassword}
-            onChange={handleComfirmPassword}
-          />
-
-          <div
-            className={`hostel-manager-password-change-update-success ${passwordUpdateSuccessfull === 'Update Successful' ? "success" : "notSuccess"}`}
-          >{passwordUpdateSuccessfull}</div>
-
-          <div className="hostel-manager-password-change-password-warning">
-            Use at least 8 characters with a mix of letters, number and symbols
-            for a stronger password
+        <div>
+          <div className="hostel-manager-password-change-header-container">
+            <img
+              src={passwordImage}
+              alt=""
+              className="hostel-manager-password-change-header-image"
+            />
+            <p className="hostel-manager-password-change-header-paragragh">
+              Change password
+            </p>
+            <p className="hostel-manager-password-change-header-second-paragraph">
+              Keep your account secure with a strong password
+            </p>
           </div>
 
-          <div className="hostel-manager-password-change-form-buttons">
+          <div className="hostel-manager-password-change-main-container">
+            <label
+              for=""
+              className="hostel-manager-password-change-input-headers"
+            >
+              OLD PASSWORD
+            </label>
             <input
-              type="submit"
-              value="Update Password"
-              className="hostel-manager-password-change-form-button-sumbit"
+              type="text"
+              className="hostel-manager-password-change-input"
+              value={hostelManagerOldpassword}
+              onChange={handleOldPassword}
             />
+
+            <label
+              htmlFor=""
+              className="hostel-manager-password-change-input-headers"
+            >
+              NEW PASSWORD
+            </label>
             <input
-              type="button"
-              value="Clear Form"
-              className="hostel-manager-password-change-form-button-clearButton"
-              onClick={handleClearForm}
-            />  
-              
+              type="text"
+              name=""
+              id=""
+              className="hostel-manager-password-change-input"
+              value={hostelMangerNewPaswword}
+              onChange={handleNewPassword}
+            />
+
+            <label
+              htmlFor=""
+              className="hostel-manager-password-change-input-headers"
+            >
+              COMFIRM PASSWORD
+            </label>
+            <input
+              type="text"
+              className="hostel-manager-password-change-input"
+              value={hostelManagerComfirmPassword}
+              onChange={handleComfirmPassword}
+            />
+
+            <div
+              className={`hostel-manager-password-change-update-success ${passwordUpdateSuccessfull === "Update Successful" ? "success" : "notSuccess"}`}
+            >
+              {passwordUpdateSuccessfull}
+            </div>
+
+            <div className="hostel-manager-password-change-password-warning">
+              Use at least 8 characters with a mix of letters, number and
+              symbols for a stronger password
+            </div>
+
+            <div className="hostel-manager-password-change-form-buttons">
+              <input
+                type="submit"
+                value="Update Password"
+                className="hostel-manager-password-change-form-button-sumbit"
+              />
+              <input
+                type="button"
+                value="Clear Form"
+                className="hostel-manager-password-change-form-button-clearButton"
+                onClick={handleClearForm}
+              />
+            </div>
           </div>
         </div>
       </form>
